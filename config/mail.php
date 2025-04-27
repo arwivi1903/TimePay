ca3e52a7d46d62d837d47a058ec22fec72606640
Bilal Sami Zahit Özgül <57416151+arwivi1903@users.noreply.github.com>
2025-04-27 21:22:52 +0300

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -1,8 +1,8 @@
 <?php
-define('SMTP_HOST', 'mail.teknikservis.club');
+define('SMTP_HOST', '');
 define('SMTP_PORT', 587);
-define('SMTP_USERNAME', 'destek@teknikservis.club');
-define('SMTP_PASSWORD', 'bjk@1903');
-define('SMTP_FROM', 'destek@teknikservis.club');
-define('SMTP_FROM_NAME', 'TimePay System');
+define('SMTP_USERNAME', '');
+define('SMTP_PASSWORD', '');
+define('SMTP_FROM', 'd');
+define('SMTP_FROM_NAME', '');
 ?>
